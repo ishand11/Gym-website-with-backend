4f7fac0d5b5d23eb2dd7085cbb4e1b48fac8efbd
ishand11 <90643358+ishand11@users.noreply.github.com>
2023-04-28 21:34:12 +0530

    Javascript file
    Used javascript, expressJS, NodeJS

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 80;
+
+//EXPRESS SPECIFIC STUFF
+app.use("/static", express.static("static")); //serving static files
+app.use(express.urlencoded());
+
+//PUG SPECIFIC STUFF
+app.set("view engine", "pug"); //set the template engine as pug
+app.set("views", path.join(__dirname, "views")); // set the views directory
+
+//ENDPOINTS
+app.get("/", (req, res) => {
+    const con = "This is the best content on the internet so far so good";
+    const params = {
+        title: "COD is the best game",
+        content: con,
+    };
+    res.status(200).render("index.pug", params); //connecting the file over here
+});
+
+app.post("/", (req, res) => {
+    name = req.body.name;
+    age = req.body.age;
+    gender = req.body.gender;
+    address = req.body.address;
+    more = req.body.more;
+    let outputToWrite = `the name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`;
+
+    fs.writeFileSync("output.txt", outputToWrite); //creating a custom file to save the submitted data in the file
+    const params = {
+        message: "Your form has been submitted successfully",
+    };
+    res.status(200).render("index.pug", params);
+});
+
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+});
